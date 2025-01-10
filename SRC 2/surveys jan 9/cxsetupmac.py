38ee63f8a2d0fc05dca66affffb7b505210ea1c5
katja <simulateconditions@gmail.com>
2025-01-10 14:33:13 -0500

    mac build file

---
+++ SRC 2/surveys jan 9/cxsetupmac.py
@@ -0,0 +1,23 @@
+import cx_Freeze as cx
+
+cx.setup(
+    name='VTSurveyProject',
+    version = '1.0',
+    author = 'Simulate Conditions LLC',
+    description = 'VT LTC Survey Download and Analysis GUI',
+    packages = ['VTSurveyProject'],
+    executables = [cx.Executable('VTSurveyProject/__main__.py',target_name='GUI')],
+    options={
+            'bdist_mac':{'bundle_name':'VTSurveyProject'},
+            'build_exe': {
+                            'packages': ['ssl','tkinter','threading','os','socket','datetime','random',                 'numpy','requests','certifi','idna','urllib3','pandas','pytz','tzdata','six','openpyxl','cv2','pytesseract','PIL','packaging','pdf2image','PyPDF2','typing_extensions'
+                       ] ,
+                            'includes':['et_xmlfile','dateutil','charset_normalizer','tkinter.ttk','tkinter.messagebox','urllib.request']
+                    }
+            }
+    
+    
+    )
+
+#No module named 'pandas.read_excel''pandas.read_excel', ,'pandas.DataFrame'
+#
